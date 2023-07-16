713a3806f3edae7c95e6630197da8375d7bf843c
Volodymyr Marchenko <Katalizator1992@gmail.com>
2023-07-16 19:53:30 +0300

    restructured_the_project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { GlobalStyle } from "./GlobalStyle";
+function App() {
+  return (
+    <>
+    Hello world!
+    <GlobalStyle/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/GlobalStyle.js
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components';
+import 'modern-normalize';
+
+export const GlobalStyle = createGlobalStyle`
+*, 
+*::before,
+*::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    list-style: none;
+    text-decoration: none;
+}
+body {
+    margin: 0;
+    /* font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif; */
+      font-family: 'Roboto', sans-serif; 
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+  img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+  }
+`
